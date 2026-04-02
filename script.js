924d7e761509af225ce006ca50a0f2371eff1afb
xxxmyf1 <xxxxmyfi@gmail.com>
2026-04-02 22:18:01 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Portfolio loaded!');
 });
+
+const skinImg = document.querySelector('.skin-img');
+const loader = document.querySelector('.skin-loader');
+
+skinImg.addEventListener('loadstart', () => {
+  skinImg.classList.add('loading');
+});
+
+skinImg.addEventListener('load', () => {
+  skinImg.classList.remove('loading');
+});
+
+skinImg.addEventListener('error', () => {
+  skinImg.classList.remove('loading');
+});
+
+
+setTimeout(() => {
+  if (!skinImg.complete) {
+    skinImg.src = 'https://minotar.net/armor/body/Steve/100.png';
+  }
+}, 5000);
